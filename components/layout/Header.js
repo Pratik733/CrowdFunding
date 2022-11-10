11adc43ff69632cff7142fe5ff6b96484f77ad1e
Pratik733 <pratik.tayade733@gmail.com>
2022-11-10 23:12:32 +0530

    class Update

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -11,29 +11,29 @@ const Header = () => {
     //   <HeaderRight />
     // </HeaderWrapper>
     <div>
-      <div class="flex flex-wrap place-items-center h-30">
-        <section class="relative mx-auto">
-          <nav class="flex justify-between bg-gray-900 text-white w-screen">
-            <div class="px-5 xl:px-12 py-6 flex w-full items-center">
-              <a class="text-3xl font-bold font-heading" href="#">
+      <div className="flex flex-wrap place-items-center h-30">
+        <section className="relative mx-auto">
+          <nav className="flex justify-between bg-gray-900 text-white w-screen">
+            <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+              <a className="text-3xl font-bold font-heading" href="#">
               <HeaderLogo />
               </a>
 
-              <div class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
+              <div className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12">
               <HeaderNav />
               </div>
 
-              <div class="hidden xl:flex items-center space-x-5 items-center">
+              <div className="hidden xl:flex items-center space-x-5 items-center">
                 
               <HeaderRight />
                 
               </div>
             </div>
 
-            <a class="xl:hidden flex mr-6 items-center" href="#">
+            <a className="xl:hidden flex mr-6 items-center" href="#">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 hover:text-gray-200"
+                className="h-6 w-6 hover:text-gray-200"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -45,15 +45,15 @@ const Header = () => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span class="flex absolute -mt-5 ml-4">
-                <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
+              <span className="flex absolute -mt-5 ml-4">
+                <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
               </span>
             </a>
-            <a class="navbar-burger self-center mr-12 xl:hidden" href="#">
+            <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 hover:text-gray-200"
+                className="h-6 w-6 hover:text-gray-200"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
